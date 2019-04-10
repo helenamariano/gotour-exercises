1065ef705fb3dbb6f374182fb3ff7682bda4d9f7
Helena Mariano <helenamariano@gmail.com>
2019-04-10 13:44:17 +1000

    complete rot13reader exercise

---
+++ rot13reader/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+var rot13Lookup = map[byte]byte{
+	'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q', 'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U',
+	'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y', 'M': 'Z', 'N': 'A', 'O': 'B', 'P': 'C',
+	'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G', 'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K',
+	'Y': 'L', 'Z': 'M',
+	'a': 'n', 'b': 'o', 'c': 'p', 'd': 'q', 'e': 'r', 'f': 's', 'g': 't', 'h': 'u',
+	'i': 'v', 'j': 'w', 'k': 'x', 'l': 'y', 'm': 'z', 'n': 'a', 'o': 'b', 'p': 'c',
+	'q': 'd', 'r': 'e', 's': 'f', 't': 'g', 'u': 'h', 'v': 'i', 'w': 'j', 'x': 'k',
+	'y': 'l', 'z': 'm',
+}
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rr *rot13Reader) Read(b []byte) (n int, err error) {
+	in := make([]byte, len(b))
+	n, err = rr.r.Read(in)
+	if err != nil {
+		return n, err
+	}
+
+	for i := 0; i < n; i++ {
+		b[i] = rot13(in[i])
+	}
+	return n, nil
+}
+
+func rot13(c byte) byte {
+	val, ok := rot13Lookup[c]
+	if !ok {
+		return c
+	}
+	return val
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
